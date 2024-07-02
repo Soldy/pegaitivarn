3b72e92cd548b1848ef5d8a9f35aff8c9502a06e
Soldy <4786022+Soldy@users.noreply.github.com>
2024-07-02 11:35:08 +0100

    timestamp to namespace
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- tools/timestamp.hpp
+++ tools/timestamp.hpp
@@ -1,6 +1,11 @@
+#ifndef __PEGAITIVARN_TIMESTAMP_
+#define __PEGAITIVARN_TIMESTAMP_
 #include <chrono>
 
+namespace pai_time { 
 int timestamp(){
     return static_cast<int>(time(NULL));
 };
+}
+#endif
 
