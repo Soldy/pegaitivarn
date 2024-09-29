2b74401ba248af3550f0c44aafe4be9167a5ec88
Soldy <4786022+Soldy@users.noreply.github.com>
2024-09-29 18:54:29 +0100

    clean up
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- hwmon/dht22.hpp
+++ hwmon/dht22.hpp
@@ -1,35 +1,40 @@
 #ifndef __PAI_HWMON_DHT22
 #define __PAI_HWMON_DHT22
-#include <set>
-#include <vector>
-#include <string>
-#include <map>
-#include <iostream>
-#include <fstream>
-#include <filesystem>
 
 namespace pai_dht22 { 
+#include <wiringPi.h>
+#include <time.h>
 namespace fs = std::filesystem;
 
+struct DhtSensor {
+  float humidity;
+  float tempeture;
+}
+
 class Reader{
+  public:
+     Reader(int pinin){
+       this->pin = pinin;
+     }
   private:
-    float humidity;
-    float tempeture;
-    float humidity;
+    pai_dht22::DhtSensor data;
+    int pin = 0;
+    int max_time 85
     void read(){
+      int data[5] = { 0, 0, 0, 0, 0 };
       uint8_t laststate = HIGH;
       uint8_t nextstate = HIGH;
       uint8_t counter  = 0;
       uint8_t j   = 0, i;
       data[0] = data[1] = data[2] = data[3] = data[4] = 0; 
-      pinMode( DHT_PIN, OUTPUT );
-      digitalWrite( DHT_PIN, LOW );
+      pinMode( this->pin, OUTPUT );
+      digitalWrite( this->pin, LOW );
       delay( 16 );//
-      pinMode( DHT_PIN, INPUT );
-      for ( i = 0; i < MAX_TIMINGS; i++ ){
+      pinMode( this->pin, INPUT );
+      for ( i = 0; i < this->max_time ; i++ ){
            counter = 0;
            while ( nextstate == laststate ){
-               nextstate = digitalRead( DHT_PIN );
+               nextstate = digitalRead( this->pin );
                counter++;
                delayMicroseconds( 2 );
                if ( counter == 255 )
@@ -45,8 +50,8 @@ class Reader{
                j++;
            }
       }
-      this->humidity = (float)((data[0] << 8) + data[1]) / 10;
-      this->tempeture = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+      this->data.humidity = (float)((data[0] << 8) + data[1]) / 10;
+      this->data.tempeture = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
     }
 };
 }
