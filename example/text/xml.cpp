a50aac540516323f2a4e8cf3352e5f066c5215d3
Soldy <4786022+Soldy@users.noreply.github.com>
2025-02-13 15:44:55 +0000

    smart xml example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example/text/xml.cpp
+++ example/text/xml.cpp
@@ -3,9 +3,40 @@
 #include <iostream>
 #include "../../text/xml.hpp"
 
-int main(){
+void title(
+  std::string title_
+){
+    std::cout << std::endl;
+    std::cout << 
+      "=========================" <<
+      std::endl;
+    std::cout << 
+      "    " << 
+      title_ << 
+      std::endl;
+    std::cout << 
+      "-------------------------" <<
+      std::endl;
+};
 
-    std::string example = "fine";
+void line(
+  std::string line_
+){
+    std::cout << 
+      " " << 
+      line_ << 
+      std::endl;
+};
+void section(
+  std::string title_,
+  std::string line_
+){
+    title(title_);
+    line(line_);
+    std::cout << std::endl;
+};
+
+int main(){
     pai_text::XmlTag test;
     test.name = "a";
     test.attributes = {
@@ -13,8 +44,10 @@ int main(){
       {"style", "nothing"},
       {"onclick", "function something"}
     };
+    section("xmlSmart no data", pai_text::xmlSmart(test));
     test.data = "some extra link text";
+    section("xmlSmart with data", pai_text::xmlSmart(test));
+    section("xml", pai_text::xml(test));
+    section("xmlSingle", pai_text::xmlSingle(test));
 
-    std::cout << pai_text::xml(test) << std::endl;
-    std::cout << pai_text::xmlSingle(test) << std::endl;
 }
