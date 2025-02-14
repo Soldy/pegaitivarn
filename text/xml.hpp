4af9c03d21fbc07c93dd587bd822533168c2a3bd
Soldy <4786022+Soldy@users.noreply.github.com>
2025-02-14 09:37:33 +0000

    smart xml implementation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- text/xml.hpp
+++ text/xml.hpp
@@ -18,9 +18,35 @@ namespace pai_text{
 struct XmlTag {
   std::string name;
   std::map<std::string, std::string> attributes;
-  std::string data;
+  std::string data = "";
 };
 
+std::string tagAtributtesValueSmart(
+  std::string value_
+){
+    if(value_.contains(' ') || value_.contains(':'))
+        return (
+          "\""+
+          value_+
+          "\""
+        );
+    return value_;
+};
+
+std::string tagAtributtesSmart(
+  std::map<std::string, std::string> attributes_
+){
+    std::string out;
+    for (const auto & [k, v]  : attributes_) {
+        out += (
+          " "+
+          k+
+          "="+
+          tagAtributtesValueSmart(v)
+        );
+    }
+    return out;
+};
 
 std::string tagAtributtes(
   std::map<std::string, std::string> attributes_
@@ -51,6 +77,19 @@ std::string xmlOpen(
   );
 };
 
+std::string xmlOpenSmart(
+  pai_text::XmlTag tag_
+){
+  return (
+    "<"+
+    tag_.name+
+    tagAtributtesSmart(
+      tag_.attributes
+    )+
+    ">"
+  );
+};
+
 std::string xmlSingle(
   pai_text::XmlTag tag_
 ){
@@ -58,6 +97,19 @@ std::string xmlSingle(
     "<"+
     tag_.name+
     tagAtributtes(
+      tag_.attributes
+    )+
+    "/>"
+  );
+};
+
+std::string xmlSingleSmart(
+  pai_text::XmlTag tag_
+){
+  return (
+    "<"+
+    tag_.name+
+    tagAtributtesSmart(
       tag_.attributes
     )+
     "/>"
@@ -81,6 +133,19 @@ std::string xml(
 ){
   return (
     xmlOpen(tag_)+
+    tag_.data+
+    xmlClose(tag_)
+  );
+};
+
+
+std::string xmlSmart(
+  pai_text::XmlTag tag_
+){
+  if (tag_.data == "")
+      return xmlSingleSmart(tag_);
+  return (
+    xmlOpenSmart(tag_)+
     tag_.data+
     xmlClose(tag_)
   );
