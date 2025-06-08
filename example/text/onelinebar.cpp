c6e7b0475dfc95cc062e21638709ef78f8f22c7e
Soldy <4786022+Soldy@users.noreply.github.com>
2025-06-08 10:33:32 +0100

    onelinebar example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ example/text/onelinebar.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "../../text/onelinebar.hpp"
+
+int main(){
+    std::cout <<
+      pai_text::onelinebar(1) <<
+      pai_text::onelinebar(75) <<
+      pai_text::onelinebar(99.01) <<
+      std::endl;
+}
