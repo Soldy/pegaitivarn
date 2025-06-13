8c88283359981bae744af90674e5df0c17f03966
Soldy <4786022+Soldy@users.noreply.github.com>
2025-06-13 13:14:25 +0100

    remove atomic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- buffer/serial.hpp
+++ buffer/serial.hpp
@@ -1,26 +1,36 @@
+/**
+ *
+ *  @author: Soldy
+ *  @email: soldy@gidigi.com
+ *  @todo: code impprovement
+ *  @desc:
+ *   This is a not thread-safe simple serializer.
+ *   The point is to force the code to run an agent
+ *  in one threat. For then,
+ *      The serializer strictly called only one threat.
+ *      A locking method is not needed
+ *      That has performance benefits.
+ *  The agents communicate with a message queue.
+**/
 #ifndef __PAI_BUFFER_SERIAL_HPP
 #define __PAI_BUFFER_SERIAL_HPP
 #include <cstdint>
-#include <atomic>
-#include <iostream>
-#include <thread>
-#include <vector>
 
 
 namespace pai_buffer{
 
 class Serial{
   private:
-     std::atomic_ullong _serial = 0;
+    uint64_t _serial = 0;
   public:
     uint64_t get(){
         this->_serial++;
         return (uint64_t) this->_serial;
     };
     bool set(
-        const uint64_t number
+        const uint64_t number_
     ){
-        this->_serial = number;
+        this->_serial = number_;
         return true;
     };
     uint64_t current(){
