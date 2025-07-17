4728e25253c01aec126467ce0d79a808ab644cee
Soldy <4786022+Soldy@users.noreply.github.com>
2025-07-17 17:44:08 +0100

    safe text for arduino
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example/text/safe.cpp
+++ example/text/safe.cpp
@@ -1,6 +1,4 @@
 
-
-#include <memory>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -9,8 +7,11 @@
 int main(){
 
     std::string example = "fine";
+    std::string example2 = example;
     std::vector<std::string> filter = {"i", "e"};
     std::cout << example << std::endl;
     pai_text::safe(example, filter);
     std::cout << example << std::endl;
+    pai_text::safe(example2, filter, "0");
+    std::cout << example2 << std::endl;
 }
--- text/safe.hpp
+++ text/safe.hpp
@@ -1,10 +1,16 @@
 /**
  *
- *  @author: Soldy 
+ *  @author: Soldy
  *  @email: soldy@gidigi.com
- *  @desc: safe char filter 
- *  Thats are a simple and safe filter. 
+ *  @desc: safe char filter
+ *  Thats are a simple, safe filter.
  *  In some situations, they perform faster than regexp.
+ *
+ *  Consider not using this functions.
+ *  It is designed for a specific purpose and 
+ *  is unnecessary for your needs.
+ *  You are seeking that:
+ *  https://en.cppreference.com/w/cpp/regex/regex_replace.html
 **/
 
 
@@ -16,6 +22,17 @@
 
 
 namespace pai_text{
+
+void safe(
+  std::string & checked,
+  std::vector<std::string> unsafe,
+  std::string replace
+){
+  for(std::string::size_type i = 0; i < checked.size(); ++i)
+    for(size_t c = 0; c < unsafe.size(); c++)
+      if(checked[i] == unsafe[c][0])
+        checked[i] = replace[0];
+};
 
 void safe(
   std::string & checked,
